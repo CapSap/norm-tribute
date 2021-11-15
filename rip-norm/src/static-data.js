51637cfdd11ede0bcb19a7d2882044f39581b5dd
CapSap <cshotam@gmail.com>
2021-11-15 22:51:49 +1100

    change api get request for better results

---
+++ rip-norm/src/static-data.js
@@ -0,0 +1,29 @@
+const tweets = [
+  {
+    Hoekstra:
+      "He was most proud of his comedy. He never wanted the diagnosis to affect the way the audience or any of his loved ones saw him, Norm was a pure comic. He once wrote that 'a joke should catch someone by surprise, it should never pander.' He certainly never pandered. Norm will be missed terribly.",
+    link: "https://deadline.com/2021/09/norm-macdonald-dead-obituary-comedian-saturday-night-live-weekend-update-anchor-was-61-1234833212/",
+  },
+  {
+    JimGaffigan:
+      "Oh my God.  We lost a legend.  Norm was punishingly funny.  A unique special point of view and completely organic.  RIPNormMacDonald",
+    link: "https://twitter.com/JimGaffigan/status/1437848321863913476",
+  },
+  {
+    conan:
+      "I am absolutely devastated about Norm Macdonald. Norm had the most unique comedic voice I have ever encountered and he was so relentlessly and uncompromisingly funny. I will never laugh that hard again. I'm so sad for all of us today.",
+    link: "https://twitter.com/ConanOBrien/status/1437863451980808192",
+  },
+  {
+    jimCarrey:
+      "My dear friend Norm MacDonald passed after a brave 10 year battle. He was one of our most precious gems. An honest and courageous comedy genius. I love him.",
+    link: "https://twitter.com/JimCarrey/status/1437855600851894273",
+  },
+  {
+    adamSandler:
+      "Every one of us loved Norm. Some of the hardest laughs of my life with this man. Most fearless funny original guy we knew. An incredible dad. A great friend. A legend. Love u pal.",
+    link: "https://twitter.com/AdamSandler/status/1437872708230938624",
+  },
+];
+
+export default tweets;
