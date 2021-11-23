df5e4623fc5d10f6282a45affa287f9e21f3da73
CapSap <cshotam@gmail.com>
2021-11-23 11:42:13 +1100

    title and image is responsive

--- rip-norm/src/components/Title.js
+++ rip-norm/src/components/Title.js
@@ -3,8 +3,8 @@ import pic from "../images/GettyImages-74699261c.webp";
 function Title() {
   return (
     <div className="title-cont">
-      <div className="text-cont">
-        <h2>Tribute to</h2>
+      <div className="title-text-cont">
+        <h2>Tribute page to</h2>
         <h1 className="title-text">RIP Norm Macdonald </h1>
         <p className="subtitle">17 October 1959 - 14 September 2021 </p>
       </div>
